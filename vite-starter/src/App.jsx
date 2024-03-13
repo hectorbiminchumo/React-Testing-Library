b61a25fe6e5f8a36eda3609e492017bb8b7e1c25
Hector Biminchumo <hector.biminchumo@externos-cl.cencosud.com>
2024-03-13 09:53:44 -0500

    tests done

--- vite-starter/src/App.jsx
+++ vite-starter/src/App.jsx
@@ -1,7 +1,29 @@
+import "./App.css";
+import { useState } from "react";
+
 function App() {
+  const [buttonColor, setButtonColor] = useState("medium-violet-red");
+  const [disabled, setDisabled] = useState(false);
+  const newButtonColor = buttonColor === "medium-violet-red" ? "midnight-blue" : "medium-violet-red";
+  const className = disabled ? 'gray' : buttonColor;
+
   return (
     <div>
-      <h1>I'm gonna learn React Testing Library</h1>
+      <button
+        className={className}
+        onClick={() => setButtonColor(newButtonColor)}
+        disabled={disabled}
+      >
+        Change to {newButtonColor}
+      </button>
+      <br />
+      <input
+        type="checkbox"
+        id="disable-button-checkbox"
+        defaultChecked={disabled}
+        onChange={(e) => setDisabled(e.target.checked)}
+      />
+      <label htmlFor="disable-button-checkbox">Disable button</label>
     </div>
   );
 }
--- vite-starter/src/App.test.jsx
+++ vite-starter/src/App.test.jsx
@@ -1,8 +1,61 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 import App from "./App";
+import { kebabCaseToTitleCase } from './helpers'
 
-test("App contains correct heading", () => {
+test("button click flow", () => {
   render(<App />);
-  const headingElement = screen.getByText(/learn react/i);
-  expect(headingElement).toBeInTheDocument();
+
+  // find an element with a role of button and text matching /blue/i
+  const buttonElement = screen.getByRole("button", {
+    name: /blue/i,
+  });
+
+  // expect the class to be mediumm-violet-red
+  expect(buttonElement).toHaveClass("medium-violet-red");
+
+  // click button
+  fireEvent.click(buttonElement);
+
+  // expect the class to be midnight-blue
+  expect(buttonElement).toHaveClass("midnight-blue");
+
+  // expect the button text to match /red/i
+  expect(buttonElement).toHaveTextContent(/red/i);
 });
+
+test("checkbox flow", () => {
+  // render app
+  render(<App />);
+
+  // find elements
+  const buttonElement = screen.getByRole("button", {
+    name: /midnight-blue/i,
+  });
+  const checkboxElement = screen.getByRole("checkbox", {
+    name: /disable button/i,
+  });
+
+  //initial conditions
+  expect(buttonElement).toBeEnabled()
+  expect(checkboxElement).not.toBeChecked()
+
+  fireEvent.click(checkboxElement)
+  expect(buttonElement).toBeDisabled()
+  expect(buttonElement).toHaveClass('gray')
+  
+  fireEvent.click(checkboxElement)
+  expect(buttonElement).toBeEnabled()
+});
+
+describe("kebabCaseToTitleCase", () => {
+  test("works for no hyphens", () => {
+    expect(kebabCaseToTitleCase("red")).toBe("Red");
+  });
+  test("works for one hyphens", () => {
+    expect(kebabCaseToTitleCase("midnight-blue")).toBe("Midnight Blue");
+  });
+  test("works for multiple hyphens", () => {
+    expect(kebabCaseToTitleCase("medium-violet-red")).toBe("Medium Violet Red");
+    
+  });
+})
