014fb297a9dfe2f3f1aba6fba3e45d69578b67ed
Hector Biminchumo <hector.biminchumo@externos-cl.cencosud.com>
2024-03-21 16:23:02 -0500

    testing grandTotal when scoops and toppings change

--- sundae-starter/src/pages/entry/OrderEntry.jsx
+++ sundae-starter/src/pages/entry/OrderEntry.jsx
@@ -1,10 +1,18 @@
 import Options from './Options'
+import { useOrderDetails } from '../../contexts/OrderDetails';
+import { formatCurrency } from '../../utilities';
+
 
 const OrderEntry = () => {
+  const { totals } = useOrderDetails();
+  const grandTotal = totals.scoops + totals.toppings
+
   return (
     <div>
+       <h1>Design Your Sundae!</h1>
         <Options optionType='scoops' />
         <Options optionType='toppings' />
+        <h2>Grand total: { formatCurrency(grandTotal)}</h2>
     </div>
   )
 }
